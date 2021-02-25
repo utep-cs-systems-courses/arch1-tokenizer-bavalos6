fc1621b568956b411cd024652e26f493e7b5225b
bavalos6 <bavalos6@miners.utep.edu>
2021-02-24 22:49:29 -0700

    Word start and terminator, and Count words

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -9,17 +9,40 @@ int non_space_char(char c){
   return !space_char(c) && c != '\0';
 }
 
-/*char *word_start(char *str){
-
+char *word_start(char *str){
+  char *word = str;
+  while(space_char(*word)){
+    word++;
+  }
 }
 
 char *word_terminator(char *word){
-
+  if(*word == '\0'){ //IF THERE IS NO WORD, RETURN EMPTY POINTER
+    return word;
+  }
+  else{
+    word = word_start(word); //LOOKING FOR NEXT WORD
+    while(non_space_char(*word) && *word != '\0'){ //LOOKING FOR THE NEXT SPACE CHAR OR NULL TERMINATOR
+      word++;  //MOVE THROUGH THE POINTER (THROUGH THE WORD)
+    }
+  }
+  return word;
 }
 
-int count_words(char *s){
-
-}*/
+int count_words(char *str){
+  str = word_start(str);
+  int count = 0;
+  if(*str != '\0'){
+    count++;
+    while(*(str = word_terminator(str)) != '\0'){
+      if(*word_start(str) == '\0'){
+	break;
+      }
+      count++;
+    }
+  }
+  return count;
+}
 
 void main(){
   char c = ' ';
